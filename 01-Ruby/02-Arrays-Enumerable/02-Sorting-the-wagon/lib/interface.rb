6eb34fa73cbbf4a04355f39da24501976f93a2de
benoitlecorre <bnt.lecorre@gmail.com>
2014-04-09 16:06:25 +0200

    deuxieme exo

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -6,9 +6,13 @@ begin
   puts students.empty? ? "Type a student name:" : "Type another student name (or press enter to finish):"
   name = gets
   name.chomp! if name
-
-  # TODO: Add the name we just gotto the students array
+  students << name if name != ""
+  # TODO: Add the name we just got to the students array
 
 end while name != ""
 
-# TODO: Call `wagon_sort` method and display the sorted student list
+v = wagon_sort(students)
+print "Congratulations! Your Wagon has #{students.length} students: -"
+v.to_s
+puts v*","
+
