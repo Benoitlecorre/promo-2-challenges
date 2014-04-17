96dcf690d1f478fcc593b42dbc9c5bccf4a104a6
benoitlecorre <bnt.lecorre@gmail.com>
2014-04-17 23:06:08 +0200

    exercise solved

--- 02-OOP/05-Dessert-inheritance/lib/dessert.rb
+++ 02-OOP/05-Dessert-inheritance/lib/dessert.rb
@@ -1,15 +1,32 @@
 class Dessert
+
+attr_reader :name, :calories
+
   def initialize(name, calories)
-  end 
-  
-  def healthy?
+    @name = name
+    @calories = calories
   end
-  
+
+  def healthy?
+    @calories < 200 ? true : false
+  end
+
   def delicious?
+    true
   end
 end
 
-class JellyBean < Dessert  
+class JellyBean < Dessert
+
+attr_reader :flavor
+
   def initialize(name, calories, flavor)
+    @name = name
+    @calories = calories
+    @flavor = flavor
+  end
+
+  def delicious?
+    @flavor == "black licorice" ? false : true
   end
 end
