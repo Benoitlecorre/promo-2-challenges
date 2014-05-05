2348f51e5d6e75904b074b445983f45756ee3671
benoitlecorre <bnt.lecorre@gmail.com>
2014-05-05 12:17:04 +0200

    Two first exercices solved

--- 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
+++ 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
@@ -8,13 +8,13 @@
 function run_challenges() {
 
   // Ex 1. Read the content of the email input
-  var email = null;  // TODO: replace null, keep the email variable.
-
+  var email = $("#email").val();
+  // TODO: replace null, keep the email variable.
 
 
   // Ex 2. Fill the content of the email input with your email
 
-
+  $("#email").val( "bnt.lecorre@gmail.com" );
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
